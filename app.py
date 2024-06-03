6eb619382aa6184c6b87f9c0552e8cce7aedd645
B0230451 <linrourou879@Gmail.com>
2024-06-03 14:49:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,26 @@
 from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import time
-import traceback
-#======python的函數庫==========
+import os
 
 app = Flask(__name__)
 static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+
 # Channel Access Token
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+
 # Channel Secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 
-# 監聽所有來自 /callback 的 Post Request
+# Handle POST requests from /callback
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -39,7 +28,7 @@ def callback():
     return 'OK'
 
 
-# 處理訊息
+# Handle text messages
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
@@ -85,19 +74,18 @@ def handle_message(event):
         "還有什麼你認為我應該知道的嗎？": "我有時覺得自己不夠好，這影響了我的自信心。"
     }
     if msg in questions_answers:
-        #print(f"{english_word} 的中文翻譯是：{words_dict[english_word]}")
-    
         line_bot_api.reply_message(event.reply_token, TextSendMessage(questions_answers[msg]))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
-       
-         
 
+
+# Handle postback events
 @handler.add(PostbackEvent)
-def handle_message(event):
+def handle_postback(event):
     print(event.postback.data)
 
 
+# Welcome message for new members
 @handler.add(MemberJoinedEvent)
 def welcome(event):
     uid = event.joined.members[0].user_id
@@ -106,9 +94,8 @@ def welcome(event):
     name = profile.display_name
     message = TextSendMessage(text=f'{name}歡迎加入')
     line_bot_api.reply_message(event.reply_token, message)
-        
-        
-import os
+
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
